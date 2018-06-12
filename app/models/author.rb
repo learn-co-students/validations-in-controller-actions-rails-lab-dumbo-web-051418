d1520704d7e524c3297162b6c66d1aa20d14a0ea
github username <github email address>
2018-06-12 19:57:18 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
+
+# 1. Add validations to `Author` such that...
+#   1. The name cannot be blank
+#   1. The e-mail is unique
