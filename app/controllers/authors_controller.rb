f6ae43e9cb0beafb458668f2159423b5f252f9bc
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-13 16:03:31 -0400

    Updated Comments

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,11 +9,13 @@ class AuthorsController < ApplicationController
   def create
     # it wants me to check by creating a new instance
     @author = Author.new(author_params)
-    # then save it
+    # then check it
     if @author.valid?
+      # then save it
       @author.save
       redirect_to author_path(@author)
     else
+      # render again if validations fail
       render :new
     end
   end
